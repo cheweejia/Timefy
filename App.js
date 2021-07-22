3960a82d90f65b7a6e4df0857000250b12f88c09
junjieteoh <junjieteoh@gmail.com>
2021-07-22 17:34:10 +0800

    Expo Eject

--- App.js
+++ App.js
@@ -1,17 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as Notifications from 'expo-notifications';
+import Permission from 'expo-permissions';
+import Constants from 'expo-constants';
 
 import Alarm from "./components/Alarm"
 import Stopwatch from "./components/Stopwatch"
 import Timer from "./components/Timer"
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+Notifications.setNotificationChannelAsync('sound', {
+  name: 'sound',
+  importance: Notifications.AndroidImportance.HIGH,
+  sound: '1.mp3', // <- Android 8.0+
+});
+
+async function schedulePushNotification() {
+  console.log('notif')
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+      sound: '1.mp3'
+    },
+    trigger: { seconds: 2, channelId: 'sound' },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
+
+
+
 function AlarmScreen() {
   return (
     <View style={styles.container}>
-      <Alarm/>
+      <Alarm />
     </View>
   );
 }
@@ -19,7 +82,7 @@ function AlarmScreen() {
 function StopwatchScreen() {
   return (
     <View style={styles.container}>
-      <Stopwatch/>
+      <Stopwatch />
     </View>
   );
 }
@@ -27,7 +90,7 @@ function StopwatchScreen() {
 function TimerScreen() {
   return (
     <View style={styles.container}>
-      <Timer/>
+      <Timer />
     </View>
   )
 }
@@ -35,6 +98,30 @@ function TimerScreen() {
 const Tab = createBottomTabNavigator()
 
 export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+
   return (
     <NavigationContainer>
       <Tab.Navigator tabBarOptions={{
@@ -52,10 +139,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
--- components/Alarm.js
+++ components/Alarm.js
@@ -11,69 +11,69 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import MathGame from './MathGame';
 import Modal from 'react-native-modal';
 import { Button } from 'react-native-elements';
-//import * as Notifications from 'expo-notifications';
+import * as Notifications from 'expo-notifications';
 import Permission from 'expo-permissions';
 import Constants from 'expo-constants';
 import { Touchable } from 'react-native';
 import PushNotification from "react-native-push-notification";
 
-// Notifications.setNotificationHandler({
-//     handleNotification: async () => ({
-//         shouldShowAlert: true,
-//         shouldPlaySound: true,
-//         shouldSetBadge: true,
-//     }),
-// });
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+    }),
+});
 
-// Notifications.setNotificationChannelAsync('sound', {
-//   name: 'sound',
-//   importance: Notifications.AndroidImportance.HIGH,
-//   sound: '1.mp3', // <- Android 8.0+
-// });
+Notifications.setNotificationChannelAsync('sound', {
+    name: 'sound channel',
+    importance: Notifications.AndroidImportance.HIGH,
+    sound: '1.wav', // <- Android 8.0+
+});
 
-// async function schedulePushNotification() {
-//     console.log('notif')
-//     await Notifications.scheduleNotificationAsync({
-//         content: {
-//             title: "You've got mail! 📬",
-//             body: 'Here is the notification body',
-//             data: { data: 'goes here' },
-//             sound: '1.mp3'
-//         },
-//         trigger: { seconds: 2   , channelId: 'sound'},
-//     });
-// }
+async function schedulePushNotification() {
+    console.log('notif')
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title: "You've got mail! 📬",
+            body: 'Here is the notification body',
+            data: { data: 'goes here' },
+            sound: '1.wav'
+        },
+        trigger: { seconds: 2, channelId: 'sound'},
+    });
+}
 
-// async function registerForPushNotificationsAsync() {
-//     let token;
-//     if (Constants.isDevice) {
-//         const { status: existingStatus } = await Notifications.getPermissionsAsync();
-//         let finalStatus = existingStatus;
-//         if (existingStatus !== 'granted') {
-//             const { status } = await Notifications.requestPermissionsAsync();
-//             finalStatus = status;
-//         }
-//         if (finalStatus !== 'granted') {
-//             alert('Failed to get push token for push notification!');
-//             return;
-//         }
-//         token = (await Notifications.getExpoPushTokenAsync()).data;
-//         console.log(token);
-//     } else {
-//         alert('Must use physical device for Push Notifications');
-//     }
+async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
 
-//     if (Platform.OS === 'android') {
-//         Notifications.setNotificationChannelAsync('default', {
-//             name: 'default',
-//             importance: Notifications.AndroidImportance.MAX,
-//             vibrationPattern: [0, 250, 250, 250],
-//             lightColor: '#FF231F7C',
-//         });
-//     }
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
 
-//     return token;
-// }
+    return token;
+}
 
 // PushNotification.configure({
 //     // (optional) Called when Token is generated (iOS and Android)
@@ -183,22 +183,22 @@ export default function Alarm() {
     const notificationListener = useRef();
     const responseListener = useRef();
 
-    // useEffect(() => {
-    //     registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
 
-    //     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-    //         setNotification(notification);
-    //     });
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
 
-    //     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-    //         console.log(response);
-    //     });
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
 
-    //     return () => {
-    //         Notifications.removeNotificationSubscription(notificationListener.current);
-    //         Notifications.removeNotificationSubscription(responseListener.current);
-    //     };
-    // }, []);
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
     ////////////////////////////////////////////////////////////
 
     const [currTime, setCurrTime] = useState(getTime(new Date()));
@@ -382,7 +382,7 @@ export default function Alarm() {
             }
             //console.log(playbackInstance);
             sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate(status))
-            await sound.loadAsync(require('./Sound/1.mp3'), status, true)
+            await sound.loadAsync(require('./Sound/11.mp3'), status, true)
 
             setPlayBackInstance(sound);
 
@@ -525,13 +525,13 @@ export default function Alarm() {
             <View style={styles.manager}>
 
 
-                {/* <TouchableOpacity
+                <TouchableOpacity
                     onPress={() => {
 
-
+                        schedulePushNotification()
                     }}>
                     <Text style={styles.timeFont}>tetestest</Text>
-                </TouchableOpacity> */}
+                </TouchableOpacity>
 
 
                 <AlarmManager
